1ec4b34d440c6c824d9ca0ec8514f84e655ee033
Habtish85 <dhubris8@gmail.com>
2023-03-16 15:45:55 -0700

    Qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - a program that prints a line, without using printf and put
+ * Return: 1 (Successful) or else it is unsuccessful
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
