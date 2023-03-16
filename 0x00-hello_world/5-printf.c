7d45087b110bb019860d2b1d1969ef8c8e06db71
Habtish85 <dhubris8@gmail.com>
2023-03-16 14:02:12 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints exactly a line  without  using put
+ * Return:  (0)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\!n");
+return (0);
+
