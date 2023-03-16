5ce98628d61c6cb158b1f6f5b55a450ec9ce7b1e
Habtish85 <dhubris8@gmail.com>
2023-03-16 15:53:30 -0700

    Qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - a program that prints a line, without using printf and put
- * Return: 1 (Successful) or else it is unsuccessful
+ * Return: 1 (Successful) or
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
