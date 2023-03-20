cb93f493e674c8c6317a387c698d03d019891eae
Habtish85 <dhubris8@gmail.com>
2023-03-20 03:53:09 -0700

    Patience persisitence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+/**
+ * main -  this prints numbers from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
+
