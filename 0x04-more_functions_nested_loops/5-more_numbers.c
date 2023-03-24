a11fed14a227fd5ed30049f28e67c80c256237a5
Habtish85 <dhubris8@gmail.com>
2023-03-24 12:24:13 -0700

    1-14 ten lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers -  prints the numbers 0-14 ten times
+ * Return: the numbers written
+ */
+
+void more_numbers(void)
+{
+int i;
+int j;
+
+for (j = 0 ; j <= 10 ; j++)
+	{
+	for (i = 0 ; i < 15 ; i++)
+		{
+		if (i > 9)
+			{
+			_putchar((i / 10) + '0');
+			}
+		_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
+
