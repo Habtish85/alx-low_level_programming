60c6a7d2784d62bc6874efdd6dbb13dc3ddfc23e
Habtish85 <dhubris8@gmail.com>
2023-04-04 12:25:15 -0700

    find char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
- *_strchr - a function that locates character in a string 
+ *_strchr - a function that locates character in a string
  * @c: the character we are looking for
  * @s: the string the character is located in
- * Return : the character, or Null if character not found
+ * Return: the character, or Null if character not found
  */
 
 char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] >= '\0' ; i++)
+	{
+		if (s[i] == c)
+		return (s + i);
+	}
+	return ('\0');
+}
